90f0b32a190a4543940ee7b646d6a39924d8ff22
GwangjinJeong <127232362+GwangjinJeong1@users.noreply.github.com>
2023-05-15 03:01:15 +0900

    Update bmalloc.c

--- bmalloc.c
+++ bmalloc.c
@@ -8,144 +8,157 @@
 bm_option bm_mode = BestFit ;
 bm_header bm_list_head = {0, 0, 0x0 } ;
 
+bm_header_ptr left_sibling ; // left
+bm_header_ptr right_sibling ; // right
+
 void * sibling (void * h)
 {
 	// TODO
-	bm_header* header = (bm_header*)h; // casting
-	size_t block_size = 1 << header->size;
-	// get the size of block
-	return ((char*)header) + sizeof(bm_header) + block_size;
-	// 현재 블록의 헤더와 다음 블록의 헤더 건너뛰기
-	// 헤더 주소에서 의심되는 형제 블록의 헤더 주소 반환
+    int size = ((bm_header *)h)->size ;
+
+	bm_header_ptr itr ;
+	bm_header_ptr left = &bm_list_head ;
+	int block_size = 1 ; // size of block
+
+	left_sibling = malloc(sizeof(bm_header)) ;	
+	right_sibling = malloc(sizeof(bm_header)) ;	
+
+	for (itr = bm_list_head.next ; itr != (bm_header *)h ; itr = itr->next) {
+		if (itr == 0x0) {
+			perror("wrong header pointer") ;
+			exit(1) ;
+		}
+		block_size += (1 << itr->size) ;
+		left_sibling = left ;
+		left = itr ;
+	}
+	
+	if ((block_size / size) % 2 == 0) {
+		left_sibling = left ;
+		right_sibling = ((bm_header *)h)->next->next ;
+		return ((bm_header *)h)->next ; 
+	} // left block
+	else {	
+		right_sibling = ((bm_header *)h)->next ;
+		return left ;
+	} // right block
 }
 
 int fitting (size_t s) 
-{
+{	
 	// TODO
-	int size_field = 4;
+	int size_field = 1;
 	while((1 << size_field) < s){
 		size_field++;
 	}
-	// 2의 거듭제곱 만큼 size_field 증가
-	return size_field;
+    if(size_field < 4 || size_field > 12)
+        return -1;
+    return size_field;
 }
 
-void* bmalloc(size_t s) {
-    int size_field = fitting(s);
-    size_t block_size = 1 << size_field;
-    // 적합한 블록 사이즈 계산
+void * bmalloc (size_t s) 
+{   
+	bm_header_ptr itr ;
+	bm_header_ptr bm_list_tail ;
+	int size_field = fitting(s) ;
 
-    bm_header* itr = &bm_list_head;
-    bm_header* prev = NULL;
+	if (bm_list_head.next == 0x0) {
+        itr = mmap(NULL, 4096,  PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS , -1, 0);
+		if (itr == MAP_FAILED) {
+			return NULL;
+		} 
+		
+		bm_list_head.next = itr ;
+		itr->used = 0 ;
+		itr->size = 12 ;
+		itr->next = 0x0 ;
+	} // Init the first block
 
-    if (bm_mode == BestFit) {
-        while (itr != NULL) {
-            if (!itr->used && itr->size >= size_field) {
-                break;
-            }
-            prev = itr;
-            itr = itr->next;
-        }
-    } else if (bm_mode == FirstFit) {
-        while (itr != NULL) {
-            if (!itr->used && itr->size >= size_field) {
-                break;
-            }
-            prev = itr;
-            itr = itr->next;
-        }
+	bm_header_ptr fitting_block = malloc(sizeof(bm_header)) ; 
+	fitting_block->size = 13 ;
+    // To search minimum block
+	
+	for (itr = bm_list_head.next ; itr != 0x0 ; itr = itr->next) {
+		if (size_field <= itr->size && itr->used == 0) {
+			if (bm_mode == FirstFit) {
+				fitting_block = itr ;
+				break ;
+			}
+			else if (itr->size < fitting_block->size) {
+				fitting_block = itr ;
+			} // BestFit
+		}
+		if (itr->next == 0x0) {
+			bm_list_tail = itr ;
+		}
+	}
+
+	if (12 < fitting_block->size) {
+		itr = mmap(NULL, 4096,  PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANONYMOUS , -1, 0);
+		if (itr == MAP_FAILED) {
+			return NULL;
+		} 
+		bm_list_tail->next = itr ;
+		itr->used = 0 ;
+		itr->size = 12 ;
+		itr->next = 0x0 ;
+		fitting_block = itr ;
+	}
+    while(size_field < fitting_block->size){
+        bm_header_ptr fitting_next = fitting_block->next ;
+
+		fitting_block->next = (bm_header_ptr) ((char *)fitting_block + (1 << (fitting_block->size - 1))) ;
+		fitting_block->next->used = 0 ;
+		fitting_block->next->size = fitting_block->size - 1 ;
+		fitting_block->next->next = fitting_next ;
+
+        fitting_block->size = fitting_block->size - 1;
     }
-
-    if (itr == NULL) {
-        bm_header* new_page = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        if (new_page == MAP_FAILED) {
-            return NULL; 
-        }
-
-        new_page->used = 0;
-        new_page->size = 12;
-        new_page->next = NULL;
-
-        if (prev != NULL) {
-            prev->next = new_page;
-        } else {
-            bm_list_head.next = new_page;
-        }
-
-        itr = new_page;
-    }
-
-    while (itr->size > size_field) {
-        
-        bm_header* new_block = (bm_header*)((char*)itr + sizeof(bm_header) + (block_size >> 1));
-        new_block->used = 0;
-        new_block->size = itr->size - 1;
-        new_block->next = itr->next;
-
-        itr->used = 1;
-        itr->size -= 1;
-        itr->next = new_block;
-    }
-
-    itr->used = 1; 
-    return ((char*)itr) + sizeof(bm_header);
+	fitting_block->used = 1 ;
+    
+	void *payload = ((bm_header_ptr)((char *)fitting_block + sizeof(bm_header)));
+    memset(payload, 0, (1 << fitting_block->size) - sizeof(bm_header));
+    // Init payload
+    return payload;
 }
-
 
 void bfree (void * p) 
 {
-	// TODO 
-	bm_header* header = (bm_header*)((char*)p - sizeof(bm_header));
-    header->used = 0;
+	// TODO
+	p = ((bm_header_ptr) ((char *)p - sizeof(bm_header))) ;
+	((bm_header *)p)->used = 0 ;
 
-    // Coalesce blocks if possible
-    bm_header* itr = &bm_list_head;
-    while (itr->next != NULL){
-        if (!itr->next->used && !header->used && itr->next->size == header->size){
-            itr->next = itr->next->next;
-            itr->size++;
-            header = itr;
-        }
-        else{
-            itr = itr->next;
-        }
-    }
+	bm_header_ptr itr ;
+	for (itr = p ; ((bm_header *)sibling(itr))->used == 0 ; itr = left_sibling->next) {
+		if (itr->size == ((bm_header *)sibling(itr))->size) { 
+			left_sibling->next->next = right_sibling ;
+			left_sibling->next->size++ ;
+		}
+		else 
+			break ;
 
-    // Unmap the entire page if no blocks are used
-    if (bm_list_head.next == NULL){
-        munmap(&bm_list_head, 4096);
-    }
+		if (left_sibling->next->size == 12){
+            if (munmap(left_sibling->next, 4096) == -1) {
+                return;
+		    }
+            left_sibling->next = right_sibling ;
+            
+            break;
+        }
+	}
 }
 
-void* brealloc(void* p, size_t s) {
-    if (p == NULL) {
-        return bmalloc(s);
-    } // p가 NULL인 경우 새로운 메모리 블록 할당
-
-    bm_header* old_header = (bm_header*)((char*)p - sizeof(bm_header));
-    size_t old_size = 1 << old_header->size;
-
-    if (s <= old_size) {
-        return p;
-    } // 요청된 크기보다 작으면 p return
-
-    void* new_block = bmalloc(s);
-    if (new_block == NULL) {
-        return NULL;
-    }
-    memcpy(new_block, p, old_size);
-    bfree(p);
-
-    return new_block;
+void * brealloc (void * p, size_t s) 
+{
+	bfree(p) ;
+	return bmalloc(s) ;
 }
-
 
 void bmconfig (bm_option opt) 
-{
-	// TODO
-	bm_mode = opt;
+{   
+    // TODO
+	bm_mode = opt ;
 }
-
 
 void bmprint () 
 {
@@ -168,21 +181,27 @@ void bmprint ()
     int total_memory = 0;
     int used_memory = 0;
     int available_memory = 0;
-    int internal_fragmentation = 0;
-
+    int total_internal_fragmentation = 0;
+    
     for(itr = bm_list_head.next; itr != 0x0; itr = itr->next){
-        total_memory += (1 << itr->size);
-        if(itr->used){
-            used_memory += (1 << itr->size);
+        int allocated_size = 1 << itr->size;
+        int requested_size = allocated_size - sizeof(bm_header);
+        int internal_fragmentation = allocated_size - requested_size;
+
+        if (itr->used == 0) // Not used block
+        {
+            available_memory += requested_size;
+            total_internal_fragmentation += internal_fragmentation;
         }
-        else{
-            available_memory += (1 << itr->size);
-            internal_fragmentation += (1 << itr->size) - sizeof(bm_header);
+        else // Used block
+        {
+            used_memory += requested_size;
         }
+        total_memory += requested_size;
     }
 
     printf("Total Memory: %d\n", total_memory);
     printf("Used Memory: %d\n", used_memory);
     printf("Available Memory: %d\n", available_memory);
-    printf("Internal Fragmentation: %d\n", internal_fragmentation);
+    printf("Internal fragmentation: %d\n", total_internal_fragmentation);
 }
